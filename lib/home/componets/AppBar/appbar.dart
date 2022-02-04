9b5097e0f579fd790eb47f77cd734e25c5f2927f
gustavokg1999 <gustavorpsantos1999@gmail.com>
2022-02-04 14:45:34 -0300

    adicionado print da tela do app

--- lib/home/componets/AppBar/appbar.dart
+++ lib/home/componets/AppBar/appbar.dart
@@ -6,51 +6,52 @@ class CustomAppBar extends StatelessWidget implements PreferredSize {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 600,
-      width: MediaQuery.of(context).size.width,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Hamburguer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      'Location',
-                      style: TextStyle(color: Colors.black45),
-                    ),
-                    Text(
-                      'Cameron St., Boston',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF53575A),
+    return SafeArea(
+      child: SizedBox(
+        height: 600,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Hamburguer(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Location',
+                        style: TextStyle(color: Colors.black45),
                       ),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 45,
-                  width: 45,
-                  decoration: const BoxDecoration(
+                      Text(
+                        'Cameron St., Boston',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF53575A),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 45,
+                    width: 45,
+                    decoration: const BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                          image: AssetImage(
-                        'assets/perfil/perfil.jpg',
-                      ))),
-                )
-              ],
-            ),
-          ],
+                        image: AssetImage(
+                          'assets/perfil/perfil.jpg',
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
